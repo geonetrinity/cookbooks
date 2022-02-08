6a2095b9db9a94f1eb6bbaa672990ccb1b1a4114
geonetrinity@gmail.com <geonetrinity@gmail.com>
2022-02-08 00:59:27 +0000

    back to templates

--- apache/recipes/server.rb
+++ apache/recipes/server.rb
@@ -9,15 +9,16 @@ package 'apache2' do
 end
 
 #-- this is only for static content
-cookbook_file '/var/www/html/index.html' do
-  source 'index.html'
-end
+#cookbook_file '/var/www/html/index.html' do
+#  source 'index.html'
+#end
 
 #-- disable this when you are using cookbook_file or file resources
-#template '/var/www/html/index.html' do
-#  source 'index.html.erb'
-#  action :create
-#end
+#-- Template is good for dynamic contents
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+  action :create
+end
 
 service 'apache2' do
   action [:enable, :start]
